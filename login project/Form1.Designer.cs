bfeb25815a7c966fb0d9af8aad627d8294ef3e1e
arsicode <arsicode@ARSICODE>
2023-11-17 01:36:12 -0800

    Add project files.

---
+++ login project/Form1.Designer.cs
@@ -0,0 +1,260 @@
+﻿namespace login_project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(6, 221);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(489, 36);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "ثبت نام";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(6, 29);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(271, 34);
+            this.textBox2.TabIndex = 2;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Enabled = false;
+            this.radioButton1.Location = new System.Drawing.Point(11, 172);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(66, 33);
+            this.radioButton1.TabIndex = 4;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "دارد";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Enabled = false;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "male",
+            "female"});
+            this.comboBox1.Location = new System.Drawing.Point(6, 112);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(271, 37);
+            this.comboBox1.TabIndex = 5;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.numericUpDown1);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.groupBox1.Size = new System.Drawing.Size(501, 263);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "ثبت نام کاربران";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(6, 72);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(271, 34);
+            this.numericUpDown1.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(383, 174);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(98, 29);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "پایان خدمت";
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Enabled = false;
+            this.radioButton2.Location = new System.Drawing.Point(205, 172);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(72, 33);
+            this.radioButton2.TabIndex = 7;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "ندارد";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(416, 120);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 29);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "جنسیت";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(444, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 29);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "سن";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(333, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(148, 29);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "نام و نام خانوادگی";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(12, 415);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(501, 164);
+            this.listBox1.TabIndex = 7;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button3);
+            this.groupBox2.Controls.Add(this.textBox1);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(12, 281);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.groupBox2.Size = new System.Drawing.Size(501, 128);
+            this.groupBox2.TabIndex = 8;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "جستجو و حذف";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(254, 73);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(241, 46);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "حذف";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(6, 33);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(489, 34);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(6, 73);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(242, 46);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "جستجو";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(525, 592);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ login project/Form1.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace login_project
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // تعریف متغیر ها
+        #region data
+        string name;
+        int age;
+        bool gender;
+        bool khedmat;
+
+        //برای ذخیره اسامی به لیستی از نوع اسمی یا استرینگ نیاز داریم
+
+        List<string> names = new List<string>();
+        List<string> searchResult = new List<string>();
+
+        
+        #endregion
+
+        #region method
+        //حالا برای انجام عملیاتمون به افعال یا متد ها نیاز داریم
+
+        // چون فعل ثبت نام ما باید خروجی بده بهمون میاییم از استرینگ استفاده میکنیم
+        string register(string n, int a, bool g, bool kh)
+        {
+            if (age >= 18)
+            {
+                if (gender == true)
+                {
+                    if (khedmat)
+                    {
+                        names.Add(n);
+                        return "تبریک آقای " + n + "شما ثبت نام شدید";
+
+                    }
+                    else
+                    {
+                        return "برای ثبت نام کارت پایان خدمت نیار دارین";
+                    }
+                }
+                else
+                {
+                    names.Add(n);
+                    return "تبریک خانم " + n + "شما ثبت نام شدید";
+                }
+            }
+            else
+            {
+                return "سن شما مناسب ثبت نام نیست";
+            }
+        }
+
+        //یک فعل برای تکراری نبودن میزاریم
+        bool check(string n)
+        {
+            foreach (var item in names)
+            {
+                if (item == n)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return false;
+        }
+
+        List<string> search(string s)
+        {
+            searchResult.Clear();
+            foreach (var item in names)
+            {
+                if (item.Contains(s))
+                {
+                    searchResult.Add(item);
+                }
+            }
+            return searchResult;
+        }
+
+        string delete(string n)
+        {
+            if (check(n))
+            {
+                name.Remove(n);
+                return "کاربر از سیستم حذف شد";
+            }
+            else
+            {
+                return "چنین کاربری در سیستم نداریم";
+            }
+        }
+
+        #endregion
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            #region set data
+            name = textBox2.Text;
+            age = Convert.ToInt32(numericUpDown1.Value);
+            if (comboBox1.Text == "مرد")
+            {
+                gender = true;
+            }
+            else
+            {
+                gender = false;
+            }
+            if (radioButton1.Checked)
+            {
+                khedmat = true;
+            }
+            else if (radioButton2.Checked)
+            {
+                khedmat = false;
+            }
+            #endregion
+
+            if (check(name))
+            {
+                MessageBox.Show("نام شما قبلا در سیستم ثبت شده است");
+            }
+            else
+            {
+               MessageBox.Show(register(name, age, gender, khedmat));
+                listBox1.DataSource = null;
+                listBox1.DataSource = names;
+                textBox2.Text = "";
+                numericUpDown1.Value = 0;
+                comboBox1.SelectedItem = null;
+                radioButton1.Checked = false;
+                radioButton2.Checked = false;
+            }
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.Text == "مرد")
+            {
+                radioButton1.Enabled = true;
+                radioButton2.Enabled = true;
+            }
+            else
+            {
+                radioButton1.Enabled = false;
+                radioButton2.Enabled = false;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //listBox1.DataSource = null;
+            //listBox1.DataSource = search(textBox1.Text);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            listBox1.DataSource = null;
+            listBox1.DataSource = search(textBox1.Text);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(delete(textBox1.Text));
+            listBox1.DataSource = null;
+            listBox1.DataSource = names;
+        }
+    }
+} 
